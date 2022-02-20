d8b575ce541774da72a75597dd04f58768e40068
huyeunten <uyeuntenhale@seattleu.edu>
2022-02-20 09:34:08 -0800

    review

--- writerreview/src/components/Post.jsx
+++ writerreview/src/components/Post.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Post.css";
 import Review from "./Review"
+
 const Post = ({ post: { title, body,
 imgUrl, author }, index }) => {
+	const writeReview = () => {
+		//alert("review");
+		<div>
+			<Review />
+		</div>
+		// return (
+		// <div>
+		// 	<h1 className="heading">Write a Review</h1>
+		//     <form onSubmit={this.handleSubmit}>
+		//         <label>
+		//             Review: <input type="text" value={this.state.value} onChange={this.handleChange}/>
+		//             <button onClick={this.handleSubmit}>Submit</button>
+		//         </label>
+		//     </form>
+		// </div>
+		// );
+	};
 
-	
 return (
 	<div className="post-container">
 	<h1 className="heading">{title}</h1>
@@ -14,7 +31,7 @@ return (
 	<div className="info">	
 		<h4>Written by: {author}</h4>
 	</div>
-	<button>
+	<button onClick={writeReview}>
               Review
             </button>
 	</div>
--- writerreview/src/components/Review.jsx
+++ writerreview/src/components/Review.jsx
@@ -1,11 +1,23 @@
 import "./Review.css";
-const Review = ({ post: { title, body,
-    imgUrl, author }, index }) => {
-      
+const Review = ({ post: { body, author }, index }) => {
+     
+    const handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+
+    const handleSubmit = () => {
+        alert("submitted");
+    }
+
     return (
         <div className="post-container">
-        <h1 className="heading">{title}</h1>
-        <img className="image" src={imgUrl} alt="post" />
+        <h1 className="heading">Write a Review</h1>
+        <form onSubmit={this.handleSubmit}>
+            <label>
+                Review: <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                <button onClick={this.handleSubmit}>Submit</button>
+            </label>
+        </form>
         <p>{body}</p>
         <div className="info">	
             <h4>Written by: {author}</h4>
